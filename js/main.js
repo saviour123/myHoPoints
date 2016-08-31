90a70d0dbdb563b6f3b3b5907bdf956450f33d91
saviour123 <profsaviour@gmail.com>
2016-08-31 15:38:57 +0000

    First Commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+//creating the map with div
+var map = L.map('map').setView([8.076,-0.967], 7);
+
+//add tiles
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//add geoJson
+var myLayer = L.geoJson(data).addTo(map);
+
+/*//create map icon
+var blueIcon = new L.Icon({iconUrl: 'img/marker-icon.png'});
+
+//the function that binds the icon to the layer
+function myIcon(feature, layer) {
+	layer.bindPopup("<h1>Hi saviour</h1>");
+	layer.setIcon(blueIcon);
+};
+
+L.geoJson(fielddata,{
+	onEachFeature: myIcon
+}).addTo(map);*/
+
+
+
+
