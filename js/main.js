3ee415eb1f35efb505c30e2094571a7fd9768bb0
saviour123 <profsaviour@gmail.com>
2016-09-01 16:17:03 +0000

    Pops appearing now

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+
+
 //creating the map with div
 var map = L.map('map').setView([6.6069, 0.4749], 14);
 
@@ -8,5 +10,22 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //add geoJson
-var myLayer = L.geoJson(data).bindLabel('saviour').addTo(map);
-L.bi
+var myLayer = L.geoJson(data, {
+	pointToLayer: function(feature, latlng){
+		return L.marker(latlng);
+	},//geoJson content appears one for each and returns the default marker
+
+
+//this piece of code does the labelling.
+	onEachFeature: function(feature, layer){
+		var popupcontent = []; //empty array
+
+		for (var i in feature.properties){
+			popupcontent.push(i + ": " + feature.properties[i]);
+		}
+		layer.bindPopup(popupcontent.join("<br />"))
+	}//end onEachFeature
+}).addTo(map);
+
+
+
